0d75903ebb2746e954218380ddd8e4f939ac27eb
Khanh Dinh An <khanh.dinh.an.k1f@fh-zwickau.de>
2022-11-19 14:39:55 +0100

    Add automatic water drain and pump

---
+++ Assets/Scripts/Hochwasserentlastung.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hochwasserentlastung : MonoBehaviour
+{
+
+    private Animator animator;
+    private ParticleSystem ps1;
+    private ParticleSystem ps2;
+    private ParticleSystem ps3;
+    private ParticleSystem ps4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        ps1 = GameObject.Find("ps1").GetComponent<ParticleSystem>();
+        ps2 = GameObject.Find("ps2").GetComponent<ParticleSystem>();
+        ps3 = GameObject.Find("ps3").GetComponent<ParticleSystem>();
+        ps4 = GameObject.Find("ps4").GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animator != null)
+        {
+            if (AnimatorIsPlaying("WaterDrain"))
+            {
+                if (!ps1.isPlaying) { 
+                    ps1.Play();
+                }
+                if (!ps2.isPlaying)
+                {
+                    ps2.Play();
+                }
+                if (!ps3.isPlaying)
+                {
+                    ps3.Play();
+                }
+                if (!ps4.isPlaying)
+                {
+                    ps4.Play();
+                }
+            }
+            else
+            {
+                if(ps1.isPlaying)
+               ps1.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+                if (ps2.isPlaying)
+                    ps2.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+                
+                if (ps3.isPlaying)
+                    ps3.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+                if (ps4.isPlaying)
+                    ps4.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+        }          
+    }
+
+    bool AnimatorIsPlaying(string stateName)
+    {
+        return AnimatorIsPlaying() &
+            animator.GetCurrentAnimatorStateInfo(0).IsName(stateName) &
+                !animator.IsInTransition(0);
+    }
+
+    bool AnimatorIsPlaying()
+    { 
+        return animator.GetCurrentAnimatorStateInfo(0).length >
+               animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+}
